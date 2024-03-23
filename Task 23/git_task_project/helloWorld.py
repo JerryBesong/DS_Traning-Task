0155481648db5fe95e07df2ce68668c8341aa4ff
Jerry Besong <153515422+JerryBesong@users.noreply.github.com>
2024-03-23 11:11:53 +0000

    T23 - Version Control and Git

---
+++ Task 23/git_task_project/helloWorld.py
@@ -0,0 +1,1 @@
+'#!/usr/bin/env python\n\n# Modified helloWorld.py to accept input from user\nuser_input = input("Please enter your message: ")\n\n# Print out the input data\nprint("You entered:", user_input)' 
